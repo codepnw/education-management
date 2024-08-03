14f1542bc14436faabb6e1f2dc3eea1e61c36252
codepnw <idchokms01@outlook.com>
2024-08-03 22:32:39 +0700

    main: add router modules

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,9 +4,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/codepnw/education/internal/handlers"
-	"github.com/codepnw/education/internal/repositories"
-	"github.com/codepnw/education/internal/usecases"
 	"github.com/codepnw/education/pkg/database"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -23,16 +20,11 @@ func main() {
 	}
 	defer db.Close()
 
-	stdRepo := repositories.NewStudentRepository(db)
-	stdUsecase := usecases.NewStudentUsecase(stdRepo)
-	stdHandler := handlers.NewStudentHandler(stdUsecase)
-
 	r := gin.Default()
-
-	r.POST("/students", stdHandler.CreateStudent)
-	r.GET("/students", stdHandler.GetAllStudents)
-	r.PATCH("/students/:id", stdHandler.UpdateStudentByID)
-	r.DELETE("/students/:id", stdHandler.DeleteStudent)
+	router := NewRouter(db, r)
+	
+	router.StudentsRouter()
+	router.TeachersRouter()
 
 	r.Run(":" + os.Getenv("PORT"))
 }
---
+++ cmd/server/router.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/codepnw/education/internal/handlers"
+	"github.com/codepnw/education/internal/repositories"
+	"github.com/codepnw/education/internal/usecases"
+	"github.com/gin-gonic/gin"
+)
+
+type RouterFactory interface {
+	StudentsRouter()
+	TeachersRouter()
+}
+
+type routerFactory struct {
+	db *sql.DB
+	e  *gin.Engine
+}
+
+func NewRouter(db *sql.DB, e *gin.Engine) RouterFactory {
+	return &routerFactory{
+		db: db,
+		e:  e,
+	}
+}
+
+func (r *routerFactory) StudentsRouter() {
+	repo := repositories.NewStudentRepository(r.db)
+	usecase := usecases.NewStudentUsecase(repo)
+	handler := handlers.NewStudentHandler(usecase)
+
+	router := r.e.Group("/students")
+
+	router.POST("/", handler.CreateStudent)
+	router.GET("/", handler.GetAllStudents)
+	router.PATCH("/:id", handler.UpdateStudentByID)
+	router.DELETE("/:id", handler.DeleteStudent)
+}
+
+func (r *routerFactory) TeachersRouter() {
+	repo := repositories.NewTeacherRepository(r.db)
+	usecase := usecases.NewTeacherUsecase(repo)
+	handler := handlers.NewTeacherHandler(usecase)
+
+	router := r.e.Group("/teachers")
+
+	router.POST("/", handler.CreateTeacher)
+	router.GET("/", handler.GetAllTeachers)
+	router.GET("/:id", handler.GetTeacherByID)
+	router.PATCH("/:id", handler.UpdateTeacher)
+	router.DELETE("/:id", handler.DeleteTeacher)
+}
